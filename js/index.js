2d21ada9640b3e37350a5db089cc0c36fb3afcb5
Faisal Akram Khan <faisalkhanfk.fk31@gmail.com>
2020-04-13 11:00:33 -0400

    commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-        
+       
         function addFriend(name) {
             const template = document.querySelector("template#friend-card-template");
             let newFriendCard = document.importNode(template.content, true);
@@ -7,6 +7,14 @@
             let randomNum = (Math.floor(Math.random() * 8)+1); 
             newFriendCard.querySelector(".friend-photo-img").setAttribute("src","img/icons/"+randomNum+".png");
             newFriendCard.querySelector(".friend-details").innerHTML = name;
+             //STEP 2
+             conn = peer.connect(name+'_1A2bcC2L');
+             // var conn = peer.connect('dc_7iun3cuzuho');
+             // on open will be launch when you successfully connect to PeerServer
+             conn.on('open', function(){
+                 // here you have conn.id
+                 conn.send('You are now connected with '+ nameTxt.innerHTML+'!');
+             });
             document.querySelector(".friends-card").appendChild(newFriendCard);
         }
         function deleteFriend(event){
@@ -28,7 +36,8 @@
             //Delete the parent from html
             parentEl.remove();
         }
-        
+        let peer;
+        let conn;
         let friendsarray = localStorage.getItem("friendNames");
         if(friendsarray == null){
             friendsarray = new Array();
@@ -43,21 +52,34 @@
         let usernamelocal = localStorage.getItem("username");
         if(usernamelocal != null){
             nameTxt.innerHTML = usernamelocal;
+        }else{
+            let myname = prompt("Enter your name (8 digits only)").substring(0,8);
+            nameTxt.innerHTML = myname;
+            localStorage.setItem("username", myname);
         }
-
+        peer = new Peer(nameTxt.innerHTML+'_1A2bcC2L');
+        peer.on('open', function(id) {
+            console.log('My peer ID is: ' + id);
+          });
         nameTxt.addEventListener("click",() => {
             let myname = prompt("Enter your name (8 digits only)").substring(0,8);
             nameTxt.innerHTML = myname;
             localStorage.setItem("username", myname);
+            peer = new Peer(nameTxt.innerHTML+'_1A2bcC2L');
         });
 
         let addFriendBtn = document.getElementById("add-friend-btn");
         addFriendBtn.addEventListener("click", () => {
+            
             let friendName = prompt("Enter your friend's name").substring(0,8);
             if(friendName != ""){
                 friendsarray.push(friendName);
                 localStorage.setItem("friendNames",JSON.stringify(friendsarray));
                 addFriend(friendName);
+
+               
+
+
             }
         });
         let elements = document.querySelectorAll(".delete-btn");
@@ -92,5 +114,12 @@
             }).then(function() { console.log("Service Worker Registered"); });
           }
         
+          
+          peer.on('connection', function(conn) {
+            conn.on('data', function(data){
+              // Will print 'hi!'
+              console.log(data);
+            });
+          });
         
         
